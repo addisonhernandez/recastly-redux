a178759bc6bd61c3fea6c308028db4873e22701a
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-09 20:53:21 -0500

    Add action object to change the list of videos

--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,12 @@
+/**
+ *
+ * @param {Object[]} videos
+ * @returns A redux object with a payload of videos to update the VideoList
+ */
 var changeVideoList = (videos) => ({
-  //TODO:  Return some action object to change the list of videos in VideoList.
+  // Return some action object to change the list of videos in VideoList.
+  type: 'CHANGE_VIDEO_LIST',
+  videos
 });
 
 export default changeVideoList;
